a192f7b60041405367e6ec913939c9f3bf025f37
Henry Dong <donghch@student.ubc.ca>
2024-01-29 20:05:04 -0800

    LCD1602 driver (partial)

---
+++ LCD1602/lcd1602.c
@@ -0,0 +1,123 @@
+/*
+ * library.c
+ *
+ * Created: 1/23/2024 4:35:00 PM
+ *  Author: diesh
+ */ 
+
+#include <xc.h>
+#include "lcd1602.h"
+
+#ifndef F_CPU
+#define F_CPU 8000000
+#endif 
+#include <util/delay.h>
+
+static void lcd1602_send_data() {
+	_delay_us(50);
+	LCD1602_E_P |= 1 << LCD1602_E_PN;
+	_delay_us(650);
+	LCD1602_E_P &= ~(1 << LCD1602_E_PN);
+	_delay_us(650);
+}
+
+static void lcd1602_set_data(uint8_t data) {
+	
+	LCD1602_D7_P = ((data >> 7) << LCD1602_D7_PN) | LCD1602_D7_P;
+	LCD1602_D6_P = ((data >> 6) << LCD1602_D6_PN) | LCD1602_D6_P;
+	LCD1602_D5_P = ((data >> 5) << LCD1602_D5_PN) | LCD1602_D5_P;
+	LCD1602_D4_P = ((data >> 4) << LCD1602_D4_PN) | LCD1602_D4_P;
+	LCD1602_D3_P = ((data >> 3) << LCD1602_D3_PN) | LCD1602_D3_P;
+	LCD1602_D2_P = ((data >> 2) << LCD1602_D2_PN) | LCD1602_D2_P;
+	LCD1602_D1_P = ((data >> 1) << LCD1602_D1_PN) | LCD1602_D1_P;
+	LCD1602_D0_P = ((data >> 0) << LCD1602_D0_PN) | LCD1602_D0_P;
+	
+}
+
+void lcd1602_setup() {
+	
+	LCD1602_D7_D |= 1 << LCD1602_D7_PN;
+	LCD1602_D6_D |= 1 << LCD1602_D6_PN;
+	LCD1602_D5_D |= 1 << LCD1602_D5_PN;
+	LCD1602_D4_D |= 1 << LCD1602_D4_PN;
+	LCD1602_D3_D |= 1 << LCD1602_D3_PN;
+	LCD1602_D2_D |= 1 << LCD1602_D2_PN;
+	LCD1602_D1_D |= 1 << LCD1602_D1_PN;
+	LCD1602_D0_D |= 1 << LCD1602_D0_PN;
+	
+}
+
+void lcd1602_clear() {
+	LCD1602_RS_P &= ~(1 << LCD1602_RS_PN);
+	lcd1602_set_data(0b1);
+	lcd1602_send_data();
+}
+
+void lcd1602_home() {
+	LCD1602_RS_P &= ~(1 << LCD1602_RS_PN);
+	lcd1602_set_data(0b10);
+	lcd1602_send_data();
+}
+
+void lcd1602_set_entry_mode(uint8_t mode) {
+	
+	uint8_t temp_data = 0b100;
+	
+	LCD1602_RS_P &= ~(1 << LCD1602_RS_PN);
+	if (mode & LCD1602_CURSOR_MOVE_RIGHT) {
+		temp_data |= 0b10;
+	} 
+	if (mode & LCD1602_DISPLAY_SHIFT_ENABLE) {
+		temp_data |= 0b1;
+	}
+	lcd1602_set_data(temp_data)
+	lcd1602_send_data();
+}
+
+void lcd1602_set_display(uint8_t options) {
+	uint8_t temp_data = 0b1000;
+	LCD1602_RS_P &= ~(1 << LCD1602_RS_PN);
+	if (options | LCD1602_DISPLAY_ON) {
+		temp_data |= 0b100;
+	}
+	if (options | LCD1602_CURSOR_ON) {
+		temp_data |= 0b10;
+	}
+	if (options | LCD1602_CURSOR_BLINK_ON) {
+		temp_data |= 0b1;
+	}
+	lcd1602_set_data(temp_data);
+	lcd1602_send_data();
+}
+
+void lcd1602_set_shift(uint8_t options) {
+	
+}
+
+void lcd1602_set_function(uint8_t options) {
+	uint8_t temp_data = 0b110000;
+	if (options | LCD1602_TWO_LINE) {
+		temp_data |= 0b1000;
+	}
+	if (options | LCD1602_5x11_FONT) {
+		temp_data |= 0b100;
+	}
+	lcd1602_set_data(temp_data);
+	lcd1602_send_data();
+}
+
+void lcd1602_set_cgram_addr(uint8_t data) {
+	lcd1602_set_data((data & 0b111111) | 0b1000000);
+	lcd1602_send_data();
+}
+
+void lcd1602_set_ddram_addr(uint8_t data) {
+	lcd1602_set_data((data & 0b1111111) | 0b10000000);
+	lcd1602_send_data();
+}
+
+void lcd1602_write_ram(uint8_t data) {
+	LCD1602_RS_P |= 1 << LCD1602_RS_PN;
+	lcd1602_set_data(data);
+	lcd1602_send_data();
+}
---
+++ LCD1602/lcd1602.h
@@ -0,0 +1,197 @@
+/*
+ * lcdLCD1602.h
+ *
+ * Created: 2024-01-23 4:36:09 PM
+ *  Author: diesh
+ */ 
+
+#include <xc.h>
+#include <inttypes.h>
+
+#ifndef _LCDLCD1602_H_
+#define _LCDLCD1602_H_
+
+
+/* Control Bus Definitions */
+
+#ifndef LCD1602_RS_D
+#define LCD1602_RS_D DDRB 
+#endif
+
+#ifndef LCD1602_RS_P
+#define LCD1602_RS_P PORTB
+#endif
+
+#ifndef LCD1602_RS_PN
+#define LCD1602_RS_PN PORTB1
+#endif
+
+#ifndef LCD1602_E_D
+#define LCD1602_E_D DDRB
+#endif
+
+#ifndef LCD1602_E_P
+#define LCD1602_E_P PORTB
+#endif
+
+#ifndef LCD1602_E_PN
+#define LCD1602_E_PN PORTB2
+#endif
+
+/* Data bus Definitions */
+
+#ifndef LCD1602_D7_D
+#define LCD1602_D7_D DDRD
+#endif
+
+#ifndef LCD1602_D7_P
+#define LCD1602_D7_P PORTD
+#endif
+
+#ifndef LCD1602_D7_PN
+#define LCD1602_D7_PN PORTD7
+#endif
+
+#ifndef LCD1602_D6_D
+#define LCD1602_D6_D DDRD
+#endif
+
+#ifndef LCD1602_D6_P
+#define LCD1602_D6_P PORTD
+#endif
+
+#ifndef LCD1602_D6_PN
+#define LCD1602_D6_PN PORTD6
+#endif
+
+#ifndef LCD1602_D5_D
+#define LCD1602_D5_D DDRD
+#endif
+
+#ifndef LCD1602_D5_P
+#define LCD1602_D5_P PORTD
+#endif
+
+#ifndef LCD1602_D5_PN
+#define LCD1602_D5_PN PORTD5
+#endif
+
+#ifndef LCD1602_D4_D
+#define LCD1602_D4_D DDRD
+#endif
+
+#ifndef LCD1602_D4_P
+#define LCD1602_D4_P PORTD
+#endif
+
+#ifndef LCD1602_D4_PN
+#define LCD1602_D4_PN PORTD4
+#endif
+
+#ifndef LCD1602_D3_D
+#define LCD1602_D3_D DDRD
+#endif
+
+#ifndef LCD1602_D3_P
+#define LCD1602_D3_P PORTD
+#endif
+
+#ifndef LCD1602_D3_PN
+#define LCD1602_D3_PN PORTD3
+#endif
+
+#ifndef LCD1602_D2_D
+#define LCD1602_D2_D DDRD
+#endif
+
+#ifndef LCD1602_D2_P
+#define LCD1602_D2_P PORTD
+#endif
+
+#ifndef LCD1602_D2_PN
+#define LCD1602_D2_PN PORTD2
+#endif
+
+#ifndef LCD1602_D2_D
+#define LCD1602_D2_D DDRD
+#endif
+
+#ifndef LCD1602_D2_P
+#define LCD1602_D2_P PORTD
+#endif
+
+#ifndef LCD1602_D2_PN
+#define LCD1602_D2_PN PORTD2
+#endif
+
+#ifndef LCD1602_D1_D
+#define LCD1602_D1_D DDRD
+#endif
+
+#ifndef LCD1602_D1_P
+#define LCD1602_D1_P PORTD
+#endif
+
+#ifndef LCD1602_D1_PN
+#define LCD1602_D1_PN PORTD1
+#endif
+
+#ifndef LCD1602_D0_D
+#define LCD1602_D0_D DDRD
+#endif
+
+#ifndef LCD1602_D0_P
+#define LCD1602_D0_P PORTD
+#endif
+
+#ifndef LCD1602_D0_PN
+#define LCD1602_D0_PN PORTD7
+#endif
+
+/* Options definitions */
+#define LCD1602_CURSOR_MOVE_LEFT 0
+#define LCD1602_CURSOR_MOVE_RIGHT 1
+
+#define LCD1602_DISPLAY_SHIFT_ENABLE 2
+
+#define LCD1602_DISPLAY_ON 4
+#define LCD1602_CURSOR_ON 8
+#define LCD1602_CURSOR_BLINK_ON 16
+
+/* Function Set Options */
+
+#define LCD1602_ONE_LINE 0
+#define LCD1602_TWO_LINE 1
+
+#define LCD1602_5x8_FONT 0
+#define LCD1602_5x11_FONT 2
+
+
+
+
+/* Function Definitions */ 
+
+void lcd1602_setup();
+
+void lcd1602_clear();
+
+void lcd1602_home();
+
+void lcd1602_set_entry_mode(uint8_t mode);
+
+void lcd1602_set_display(uint8_t options);
+
+void lcd1602_set_shift(uint8_t options);
+
+void lcd1602_set_function(uint8_t options);
+
+void lcd1602_set_cgram_addr(uint8_t data);
+
+void lcd1602_set_ddram_addr(uint8_t data);
+
+void lcd1602_write_ram(uint8_t data);
+
+uint8_t lcd1602_read_ram();
+
+
+#endif /* _LCDLCD1602_H_ */
